68567c43a50710f13144ef0546416b68692f4a01
yoshitokomiya <yoshitokomiya@ccl107im04.local>
2017-05-01 16:43:22 -0700

    Initial Commit

---
+++ Assignment#7/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment#7
+//
+//  Created by yoshitokomiya on 2017-05-01.
+//  Copyright © 2017 yoshitokomiya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
